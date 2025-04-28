159b3f9e25c3f328ba45495da640b23c254d07f2
TranTienAnh2012 <tienanhtran755@gmail.com>
2025-04-28 09:15:50 +0700

    Day02

---
+++ Ttalession02/Ttalession02/Controllers/TtaProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ttalession02.Controllers
+{
+    public class TtaProductController : Controller
+    {
+        public IActionResult TtaIndex()
+        {
+            ViewData["messageData"] = "Dữ liệu từ ViewData";
+            ViewBag.messageData = "Dữ liệu từ ViewBag";
+            TempData["messageData"] = "Dữ liệu từ TempData";
+            return View();
+        }
+   
+
+    }
+}
---
+++ Ttalession02/Ttalession02/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ttalession02.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
