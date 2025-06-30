9633c8b8d22bb91b3eece5cfb06aff978750b503
TranTienAnh2012 <tienanhtran755@gmail.com>
2025-06-30 09:57:18 +0700

    Day 11 Diem Thanh Phan

---
+++ TtaLesson11/TtaLesson11/Controllers/TtaEmployeesController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TtaLesson11.Models;
+
+namespace TtaLesson11.Controllers
+{
+    public class TtaEmployeesController : Controller
+    {
+        private readonly DemoTranTienAnh2310900005Context _context;
+
+        public TtaEmployeesController(DemoTranTienAnh2310900005Context context)
+        {
+            _context = context;
+        }
+
+        // GET: TtaEmployees
+        public async Task<IActionResult> TtaIndex()
+        {
+            return View(await _context.TtaEmployees.ToListAsync());
+        }
+
+        // GET: TtaEmployees/Details/5
+        public async Task<IActionResult> TtaDetails(int? Ttaid)
+        {
+            if (Ttaid == null)
+            {
+                return NotFound();
+            }
+
+            var ttaEmployee = await _context.TtaEmployees
+                .FirstOrDefaultAsync(m => m.TtaEmpId == Ttaid);
+            if (ttaEmployee == null)
+            {
+                return NotFound();
+            }
+
+            return View(ttaEmployee);
+        }
+
+        // GET: TtaEmployees/Create
+        public IActionResult TtaCreate()
+        {
+            return View();
+        }
+
+        // POST: TtaEmployees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> TtaCreate([Bind("TtaEmpId,TtaEmpName,TtaEmpLevel,TtaEmpStartDate,TtaEmpStatus")] TtaEmployee ttaEmployee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ttaEmployee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(TtaIndex));
+            }
+            return View(ttaEmployee);
+        }
+
+        // GET: TtaEmployees/Edit/5
+        public async Task<IActionResult> TtaEdit(int? Ttaid)
+        {
+            if (Ttaid == null)
+            {
+                return NotFound();
+            }
+
+            var ttaEmployee = await _context.TtaEmployees.FindAsync(Ttaid);
+            if (ttaEmployee == null)
+            {
+                return NotFound();
+            }
+            return View(ttaEmployee);
+        }
+
+        // POST: TtaEmployees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> TtaEdit(int Ttaid, [Bind("TtaEmpId,TtaEmpName,TtaEmpLevel,TtaEmpStartDate,TtaEmpStatus")] TtaEmployee ttaEmployee)
+        {
+            if (Ttaid != ttaEmployee.TtaEmpId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ttaEmployee);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TtaEmployeeExists(ttaEmployee.TtaEmpId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(TtaIndex));
+            }
+            return View(ttaEmployee);
+        }
+
+        // GET: TtaEmployees/Delete/5
+        public async Task<IActionResult> TtaDelete(int? Ttaid)
+        {
+            if (Ttaid == null)
+            {
+                return NotFound();
+            }
+
+            var ttaEmployee = await _context.TtaEmployees
+                .FirstOrDefaultAsync(m => m.TtaEmpId == Ttaid);
+            if (ttaEmployee == null)
+            {
+                return NotFound();
+            }
+
+            return View(ttaEmployee);
+        }
+
+        // POST: TtaEmployees/Delete/5
+        [HttpPost, ActionName("TtaDelete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> TtaDeleteConfirmed(int Ttaid)
+        {
+            var ttaEmployee = await _context.TtaEmployees.FindAsync(Ttaid);
+            if (ttaEmployee != null)
+            {
+                _context.TtaEmployees.Remove(ttaEmployee);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(TtaIndex));
+        }
+
+        private bool TtaEmployeeExists(int Ttaid)
+        {
+            return _context.TtaEmployees.Any(e => e.TtaEmpId == Ttaid);
+        }
+    }
+}
---
+++ TtaLesson11/TtaLesson11/Models/DemoTranTienAnh2310900005Context.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TtaLesson11.Models;
+
+public partial class DemoTranTienAnh2310900005Context : DbContext
+{
+    public DemoTranTienAnh2310900005Context()
+    {
+    }
+
+    public DemoTranTienAnh2310900005Context(DbContextOptions<DemoTranTienAnh2310900005Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TtaEmployee> TtaEmployees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("server=LAPTOP-F48PQH12\\TIENANH1; Database=DemoTranTienAnh_2310900005; uid=TtaAdmin; PWD=tienanh2005; MultipleActiveResultSets=True; TrustServerCertificate=True ");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TtaEmployee>(entity =>
+        {
+            entity.HasKey(e => e.TtaEmpId).HasName("PK__TtaEmplo__F10E50181E679116");
+
+            entity.ToTable("TtaEmployee");
+
+            entity.Property(e => e.TtaEmpLevel).HasMaxLength(50);
+            entity.Property(e => e.TtaEmpName).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
