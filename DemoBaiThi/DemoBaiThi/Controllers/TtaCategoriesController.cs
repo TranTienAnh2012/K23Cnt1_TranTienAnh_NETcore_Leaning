0ad1150a260f63efd1c7d5bd7b42234207829896
TranTienAnh2012 <tienanhtran755@gmail.com>
2025-07-10 03:26:36 +0700

    App demo bai thi

---
+++ DemoBaiThi/DemoBaiThi/Controllers/TtaCategoriesController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DemoBaiThi.Models;
+
+namespace DemoBaiThi.Controllers
+{
+    public class TtaCategoriesController : Controller
+    {
+        private readonly TtaBookContext _context;
+
+        public TtaCategoriesController(TtaBookContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TtaCategories
+        public async Task<IActionResult> TtaIndex(string keyword)
+        {
+            var categories = _context.Categories.AsQueryable();
+
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                categories = categories.Where(c => c.CategoryName.Contains(keyword));
+            }
+
+            return View(await categories.OrderBy(c => c.CategoryId).ToListAsync());
+        }
+
+        // GET: TtaCategories/Details/5
+        public async Task<IActionResult> TtaDetails(int? TtaId)
+        {
+            if (TtaId == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.CategoryId == TtaId);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // GET: TtaCategories/Create
+        public IActionResult TtaCreate()
+        {
+            return View();
+        }
+
+        // POST: TtaCategories/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> TtaCreate([Bind("CategoryId,CategoryName")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(TtaIndex));
+            }
+            return View(category);
+        }
+
+        // GET: TtaCategories/Edit/5
+        public async Task<IActionResult> TtaEdit(int? TtaId)
+        {
+            if (TtaId == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FindAsync(TtaId);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // POST: TtaCategories/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> TtaEdit(int TtaId, [Bind("CategoryId,CategoryName")] Category category)
+        {
+            if (TtaId != category.CategoryId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(category);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryExists(category.CategoryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(TtaIndex));
+            }
+            return View(category);
+        }
+
+        // GET: TtaCategories/Delete/5
+        public async Task<IActionResult> TtaDelete(int? TtaId)
+        {
+            if (TtaId == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.CategoryId == TtaId);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: TtaCategories/Delete/5
+        [HttpPost, ActionName("TtaDelete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> TtaDeleteConfirmed(int TtaId)
+        {
+            var category = await _context.Categories.FindAsync(TtaId);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(TtaIndex));
+        }
+
+        private bool CategoryExists(int TtaId)
+        {
+            return _context.Categories.Any(e => e.CategoryId == TtaId);
+        }
+    }
+}
---
+++ DemoBaiThi/DemoBaiThi/Program.cs
@@ -0,0 +1,34 @@
+using DemoBaiThi.Models;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+var connectionString = builder.Configuration.GetConnectionString("TtaBook");
+builder.Services.AddDbContext<TtaBookContext>(x => x.UseSqlServer(connectionString));
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/TtaHome/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=TtaHome}/{action=TtaIndex}/{Ttaid?}");
+
+app.Run();
